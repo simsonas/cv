0a6a846abf64d1c2201eca8e268657ffc218031c
Darius Simsonas <simsonas.darius@gmail.com>
2026-07-26 23:09:03 +0300

    added lines

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,7 @@
 import type { MetadataRoute } from "next";
+// Add these two lines at the top of src/app/sitemap.ts
+export const dynamic = "force-static";
+export const revalidate = false;
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://simsonas.github.io/cv";
