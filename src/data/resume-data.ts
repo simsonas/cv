5b1b48d0e6ce60c1252488d38f6e9ca005cfdb00
Darius Simsonas <simsonas.darius@gmail.com>
2026-07-29 21:42:55 +0300

    added certs

--- src/data/resume-data.ts
+++ src/data/resume-data.ts
@@ -142,39 +142,39 @@ export const RESUME_DATA: ResumeData = {
   ],
   certifications: [
     {
-      name: "AWS Certified Cloud Practitioner",
-      url: "https://www.credly.com/badges/example",
+      name: "Microsoft Certified: Azure Solutions Architect Expert",
+      url: "https://learn.microsoft.com/en-us/users/dariussimsonas-9644/credentials/2287d4b183ea2ce4",
+      date: "2022",
+    },
+      {
+      name: "Microsoft Certified: DevOps Engineer Expert",
+      url: "https://learn.microsoft.com/en-us/users/dariussimsonas-9644/credentials/a240b7cade3dc68b",
+      date: "2026",
+    },
+      {
+      name: "Microsoft Certified: Fabric Data Engineer Associate",
+      url: "https://learn.microsoft.com/en-us/users/dariussimsonas-9644/credentials/eed2090f175a7871",
+      date: "2025",
+    },
+      {
+      name: "Microsoft Certified: Azure Administrator Associate",
+      url: "https://learn.microsoft.com/en-us/users/dariussimsonas-9644/credentials/e764d0e7ba35f1d0",
+      date: "2022",
+    },
+      {
+      name: "Databricks Certified: Associate Developer for Apache Spark 3.0",
+      url: "https://credentials.databricks.com/ac02c3a1-378a-456d-985b-7aa9474cbe33#acc.OZnXqcvM",
+      date: "2022",
+    },
+      {
+      name: "Microsoft Certified: Fabric Analytics Engineer Associate",
+      url: "https://learn.microsoft.com/en-us/users/dariussimsonas-9644/credentials/d81c9f261a3baf93",
       date: "2024",
     },
       {
-      name: "AWS Certified Cloud Practitioner",
-      url: "https://www.credly.com/badges/example",
-      date: "2024",
-    },
-      {
-      name: "AWS Certified Cloud Practitioner",
-      url: "https://www.credly.com/badges/example",
-      date: "2024",
-    },
-      {
-      name: "AWS Certified Cloud Practitioner",
-      url: "https://www.credly.com/badges/example",
-      date: "2024",
-    },
-      {
-      name: "AWS Certified Cloud Practitioner",
-      url: "https://www.credly.com/badges/example",
-      date: "2024",
-    },
-      {
-      name: "AWS Certified Cloud Practitioner",
-      url: "https://www.credly.com/badges/example",
-      date: "2024",
-    },
-      {
-      name: "AWS Certified Cloud Practitioner",
-      url: "https://www.credly.com/badges/example",
-      date: "2024",
+      name: "PCEP Certified Entry-Level Python Programmer",
+      url: "https://verify.openedg.org/?id=TYrz.qwnj.LSMn",
+      date: "2025",
     },
   ],
 } as const;
